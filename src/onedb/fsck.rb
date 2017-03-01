6ee8aa0a1c7fb23fb76a3ab116a9bedab8ac4116
Javi Fontan <jfontan@opennebula.org>
2017-03-01 17:38:40 +0100

    Extract check_datastores

--- src/onedb/fsck.rb
+++ src/onedb/fsck.rb
@@ -517,6 +517,96 @@ EOT
         end
     end
 
+    def check_datastores
+        datastore = {}
+
+        @db.fetch("SELECT oid, name FROM datastore_pool") do |row|
+            datastore[row[:oid]] = {:name => row[:name], :images => []}
+        end
+
+        images_fix = {}
+
+        @db.fetch("SELECT oid,body FROM image_pool") do |row|
+            doc = Document.new(row[:body])
+
+            ds_id = doc.root.get_text('DATASTORE_ID').to_s.to_i
+            ds_name = doc.root.get_text('DATASTORE')
+
+            if ds_id != -1
+                ds_entry = datastore[ds_id]
+
+                if ds_entry.nil?
+                    log_error("Image #{row[:oid]} has datastore #{ds_id}, but it does not exist. The image is probably unusable, and needs to be deleted manually:\n"<<
+                        "  * The image contents should be deleted manually:\n"<<
+                        "    #{doc.root.get_text('SOURCE')}\n"<<
+                        "  * The DB entry can be then deleted with the command:\n"<<
+                        "    DELETE FROM image_pool WHERE oid=#{row[:oid]};\n"<<
+                        "  * Run fsck again.\n", false)
+                else
+                    if ds_name != ds_entry[:name]
+                        log_error("Image #{row[:oid]} has a wrong name for datastore #{ds_id}, #{ds_name}. It will be changed to #{ds_entry[:name]}")
+
+                        doc.root.each_element('DATASTORE') do |e|
+                            e.text = ds_entry[:name]
+                        end
+
+                        images_fix[row[:oid]] = doc.root.to_s
+                    end
+
+                    ds_entry[:images] << row[:oid]
+                end
+            end
+        end
+
+        @db.transaction do
+            images_fix.each do |id, body|
+                @db[:image_pool].where(:oid => id).update(:body => body)
+            end
+        end
+
+        log_time()
+
+        @db.run "CREATE TABLE datastore_pool_new (oid INTEGER PRIMARY KEY, name VARCHAR(128), body MEDIUMTEXT, uid INTEGER, gid INTEGER, owner_u INTEGER, group_u INTEGER, other_u INTEGER);"
+
+        @db.transaction do
+            @db.fetch("SELECT * from datastore_pool") do |row|
+                ds_id = row[:oid]
+                doc = Document.new(row[:body])
+
+                images_elem = doc.root.elements.delete("IMAGES")
+
+                images_new_elem = doc.root.add_element("IMAGES")
+
+                datastore[ds_id][:images].each do |id|
+                    id_elem = images_elem.elements.delete("ID[.=#{id}]")
+
+                    if id_elem.nil?
+                        log_error(
+                            "Image #{id} is missing from Datastore #{ds_id} "<<
+                            "image id list")
+                    end
+
+                    images_new_elem.add_element("ID").text = id.to_s
+                end
+
+                images_elem.each_element("ID") do |id_elem|
+                    log_error(
+                        "Image #{id_elem.text} is in Datastore #{ds_id} "<<
+                        "image id list, but it should not")
+                end
+
+
+                row[:body] = doc.root.to_s
+
+                # commit
+                @db[:datastore_pool_new].insert(row)
+            end
+        end
+
+        # Rename table
+        @db.run("DROP TABLE datastore_pool")
+        @db.run("ALTER TABLE datastore_pool_new RENAME TO datastore_pool")
+    end
 
     ########################################################################
     # Acl
@@ -661,94 +751,7 @@ EOT
         # IMAGE/DATASTORE
         ########################################################################
 
-        datastore = {}
-
-        @db.fetch("SELECT oid, name FROM datastore_pool") do |row|
-            datastore[row[:oid]] = {:name => row[:name], :images => []}
-        end
-
-        images_fix = {}
-
-        @db.fetch("SELECT oid,body FROM image_pool") do |row|
-            doc = Document.new(row[:body])
-
-            ds_id = doc.root.get_text('DATASTORE_ID').to_s.to_i
-            ds_name = doc.root.get_text('DATASTORE')
-
-            if ds_id != -1
-                ds_entry = datastore[ds_id]
-
-                if ds_entry.nil?
-                    log_error("Image #{row[:oid]} has datastore #{ds_id}, but it does not exist. The image is probably unusable, and needs to be deleted manually:\n"<<
-                        "  * The image contents should be deleted manually:\n"<<
-                        "    #{doc.root.get_text('SOURCE')}\n"<<
-                        "  * The DB entry can be then deleted with the command:\n"<<
-                        "    DELETE FROM image_pool WHERE oid=#{row[:oid]};\n"<<
-                        "  * Run fsck again.\n", false)
-                else
-                    if ds_name != ds_entry[:name]
-                        log_error("Image #{row[:oid]} has a wrong name for datastore #{ds_id}, #{ds_name}. It will be changed to #{ds_entry[:name]}")
-
-                        doc.root.each_element('DATASTORE') do |e|
-                            e.text = ds_entry[:name]
-                        end
-
-                        images_fix[row[:oid]] = doc.root.to_s
-                    end
-
-                    ds_entry[:images] << row[:oid]
-                end
-            end
-        end
-
-        @db.transaction do
-            images_fix.each do |id, body|
-                @db[:image_pool].where(:oid => id).update(:body => body)
-            end
-        end
-
-        log_time()
-
-        @db.run "CREATE TABLE datastore_pool_new (oid INTEGER PRIMARY KEY, name VARCHAR(128), body MEDIUMTEXT, uid INTEGER, gid INTEGER, owner_u INTEGER, group_u INTEGER, other_u INTEGER);"
-
-        @db.transaction do
-            @db.fetch("SELECT * from datastore_pool") do |row|
-                ds_id = row[:oid]
-                doc = Document.new(row[:body])
-
-                images_elem = doc.root.elements.delete("IMAGES")
-
-                images_new_elem = doc.root.add_element("IMAGES")
-
-                datastore[ds_id][:images].each do |id|
-                    id_elem = images_elem.elements.delete("ID[.=#{id}]")
-
-                    if id_elem.nil?
-                        log_error(
-                            "Image #{id} is missing from Datastore #{ds_id} "<<
-                            "image id list")
-                    end
-
-                    images_new_elem.add_element("ID").text = id.to_s
-                end
-
-                images_elem.each_element("ID") do |id_elem|
-                    log_error(
-                        "Image #{id_elem.text} is in Datastore #{ds_id} "<<
-                        "image id list, but it should not")
-                end
-
-
-                row[:body] = doc.root.to_s
-
-                # commit
-                @db[:datastore_pool_new].insert(row)
-            end
-        end
-
-        # Rename table
-        @db.run("DROP TABLE datastore_pool")
-        @db.run("ALTER TABLE datastore_pool_new RENAME TO datastore_pool")
+        check_datastores()
 
         log_time()
 
